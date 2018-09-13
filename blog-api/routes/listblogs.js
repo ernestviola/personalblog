0486af4df82a2b2cfa504e3e32a1859f84022512
Ernest <ernest.viol@gmail.com>
2018-09-13 15:03:32 -0700

    Finished list and get blogs

--- blog-api/routes/listblogs.js
+++ blog-api/routes/listblogs.js
@@ -3,30 +3,28 @@
 const AWS = require('aws-sdk');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+};
 
 module.exports.main = (event, context, callback) => {
-    const params = {
-        TableName: process.env.DYNAMODB_TABLE,
-        Key: {
-            id: event.pathParameters.id
-        },
-    };
-
-    dynamoDb.get(params, (error, result) => {
+    dynamoDb.scan(params, (error, result) => {
         if (error) {
             console.error(error);
-            callback(null, {
+            const errorMessage = {
                 statusCode: error.statusCode || 501,
                 headers: { 'Content-Type': 'text/plain' },
-                body: 'Couldn\'t fetch',
-            });
+                body: 'Couldn\'t list',
+            };
+
+            callback(null, errorMessage);
             return;
         }
-
         const response = {
             statusCode: 200,
-            body: JSON.stringify(result.Item),
+            body: JSON.stringify(result.Items),
         };
         callback(null, response);
+
     })
-};
+}
