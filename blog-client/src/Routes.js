b9a67ef48cb9ce10bbff0a01d20a7636999d7e52
Ernest <ernest.viol@gmail.com>
2018-10-19 16:17:09 -0700

    Added notfound pages and updated routes

--- blog-client/src/Routes.js
+++ blog-client/src/Routes.js
@@ -6,10 +6,12 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import BlogHome from './containers/BlogHome';
 import Projects from './containers/Projects';
+import NotFound from './containers/NotFound';
 
 export default () =>
     <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/blog" exact component={BlogHome} />
         <Route path="/projects" exact component={Projects} />
+        <Route component={NotFound} />
     </Switch>;
